00b0e8de9b89516fba3bc53b6570230bdb7aee40
AchmadZackyGZ <achmadzacky458@gmail.com>
2026-07-10 14:46:12 +0700

    membuat logic untuk auth disini

--- backend-api/Modules/Auth/app/Http/Controllers/AuthController.php
+++ backend-api/Modules/Auth/app/Http/Controllers/AuthController.php
@@ -3,10 +3,65 @@
 namespace Modules\Auth\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Modules\Auth\Http\Requests\LoginRequest;
+use Modules\Auth\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
 {
+
+    public function register(RegisterRequest $request): JsonResponse
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'mahasiswa', // Default role sesuai PRD
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Registrasi berhasil. Silakan cek email untuk verifikasi.', // Nanti kita tambahkan logic email
+            'access_token' => $token,
+            'user' => $user
+        ]);
+    }
+
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Email atau password salah.'
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login berhasil.',
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'user' => $user
+        ]);
+    }
+
+    // Fungsi tambahan untuk Logout (hanya bisa diakses jika punya token)
+    public function logout(Request $request): JsonResponse
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logout berhasil.'
+        ]);
+    }
+
+
     /**
      * Display a listing of the resource.
      */
