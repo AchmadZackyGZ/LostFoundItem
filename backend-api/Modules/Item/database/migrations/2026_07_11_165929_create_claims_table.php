d62ed8c57bf1faa3ebf9810b52fd82600314c2c8
AchmadZackyGZ <achmadzacky458@gmail.com>
2026-07-12 00:55:17 +0700

    membuat kolom kolom di table_claims di migration

--- backend-api/Modules/Item/database/migrations/2026_07_11_165929_create_claims_table.php
+++ backend-api/Modules/Item/database/migrations/2026_07_11_165929_create_claims_table.php
@@ -12,8 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('claims', function (Blueprint $table) {
-            $table->id();
-            
+            $table->uuid('id')->primary();
+
+            // Relasi ke barang yang diklaim dan siapa yang mengklaim
+            $table->foreignUuid('item_id')->constrained('items')->cascadeOnDelete();
+            $table->foreignUuid('user_id')->constrained('users')->cascadeOnDelete();
+
+            // Bukti dari pengklaim
+            $table->text('proof_description');
+            $table->string('proof_image_path')->nullable(); // Foto bukti tambahan
+
+            // Status validasi Admin
+            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
+
             $table->timestamps();
         });
     }
