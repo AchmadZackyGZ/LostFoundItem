ecf4b6eda1aaefd2003d4ae435ec1a4bf6e3f379
AchmadZackyGZ <achmadzacky458@gmail.com>
2026-07-12 00:09:51 +0700

    membuat table caetgories

--- backend-api/Modules/Item/database/migrations/2026_07_11_165537_create_categories_table.php
+++ backend-api/Modules/Item/database/migrations/2026_07_11_165537_create_categories_table.php
@@ -12,8 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('slug')->unique(); // Untuk URL ramah SEO/Pencarian agar lebih enak nanti di FE
             $table->timestamps();
         });
     }
