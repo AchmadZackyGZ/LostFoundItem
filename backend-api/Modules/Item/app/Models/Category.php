5d2de819f2497e2484ec35522c6a2ba4cb9f6165
AchmadZackyGZ <achmadzacky458@gmail.com>
2026-07-12 13:02:09 +0700

    feat(item): add uuid, fillable fields and relations
    add HasUuids trait to enable UUID primary keys for Category model
    update $fillable to allow mass assignment of name and slug
    add hasMany Eloquent relationship between Category and Item

--- backend-api/Modules/Item/app/Models/Category.php
+++ backend-api/Modules/Item/app/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Item\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 // use Modules\Item\Database\Factories\CategoryFactory;
@@ -9,11 +10,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Category extends Model
 {
     use HasFactory;
+    use HasUuids;
 
     /**
      * The attributes that are mass assignable.
      */
-    protected $fillable = [];
+    protected $fillable = ['name', 'slug'];
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
 
     // protected static function newFactory(): CategoryFactory
     // {
