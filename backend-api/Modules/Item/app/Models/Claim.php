efe52e5b4d9ee6b4a4db0c9a72ef2b3e226ef5f3
AchmadZackyGZ <achmadzacky458@gmail.com>
2026-07-12 13:20:22 +0700

    menambahkan function relasi intenal module Item dengan Claim itu belongsTo Item dan juga menambahakn beberapa attribute fillable

--- backend-api/Modules/Item/app/Models/Claim.php
+++ backend-api/Modules/Item/app/Models/Claim.php
@@ -2,6 +2,8 @@
 
 namespace Modules\Item\Models;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 // use Modules\Item\Database\Factories\ClaimFactory;
@@ -9,11 +11,24 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Claim extends Model
 {
     use HasFactory;
+    use HasUuids;
 
     /**
      * The attributes that are mass assignable.
      */
-    protected $fillable = [];
+    protected $fillable = [
+        'item_id',
+        'user_id', // Sama, hanya simpan ID, tanpa relasi ke Model User
+        'proof_description',
+        'proof_image_path',
+        'status'
+    ];
+
+    // Relasi Internal Modul: Komentar ini ada di barang mana
+    public function item()
+    {
+        return $this->belongsTo(Item::class);
+    }
 
     // protected static function newFactory(): ClaimFactory
     // {
