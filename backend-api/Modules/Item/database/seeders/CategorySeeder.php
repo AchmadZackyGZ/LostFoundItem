96813c9f96508c99e477a5acce2637bf485dc389
AchmadZackyGZ <achmadzacky458@gmail.com>
2026-07-12 14:27:47 +0700

    membuat file categorySeeder dan ItemDatbaseSeeder di module Item lalu membuat kode untuk apa saja yang mau di seed ke DB

---
+++ backend-api/Modules/Item/database/seeders/CategorySeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Modules\Item\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Modules\Item\Models\Category;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            'Barang Elektronik',
+            'Dokumen & Surat',
+            'Dompet & Uang',
+            'Kunci',
+            'Pakaian & Aksesoris',
+            'Lainnya'
+        ];
+
+        foreach ($categories as $category) {
+            Category::create([
+                'id' => Str::uuid(),
+                'name' => $category,
+                'slug' => Str::slug($category)
+            ]);
+        }
+    }
+}
--- backend-api/Modules/Item/database/seeders/ItemDatabaseSeeder.php
+++ backend-api/Modules/Item/database/seeders/ItemDatabaseSeeder.php
@@ -11,6 +11,8 @@ class ItemDatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call([]);
+        $this->call([
+            CategorySeeder::class,
+        ]);
     }
 }
