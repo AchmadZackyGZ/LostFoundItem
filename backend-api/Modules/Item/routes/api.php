e5a10cf0319a57a1153aeef27e50f90bc3a4eb89
AchmadZackyGZ <achmadzacky458@gmail.com>
2026-07-14 14:45:12 +0700

    menambahkan 2 route api gateway di route api module Item nah saya menambahkan get dan post untuk lihat detail item dan kirim post komen atau discussion

--- backend-api/Modules/Item/routes/api.php
+++ backend-api/Modules/Item/routes/api.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Item\Http\Controllers\ItemController;
+use Modules\Item\Http\Controllers\DiscussionController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
     Route::apiResource('items', ItemController::class)->names('item');
     Route::post('/', [ItemController::class, 'store']); // buat laporan 
     Route::get('/', [ItemController::class, 'index']); // ambil semua laporan 
+
+    Route::get('/{id}', [ItemController::class, 'show']); // Lihat detail barang & komentar
+    Route::post('/{id}/discussions', [DiscussionController::class, 'store']); // Kirim komentar
 });
