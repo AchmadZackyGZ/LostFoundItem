ff39862b8d266f78c3803613a0d99cac5d02bb91
AchmadZackyGZ <achmadzacky458@gmail.com>
2026-07-18 10:20:38 +0700

    menambahakn seluruh api gateway route yang dibuat function method di chatCotnroler logic

--- backend-api/Modules/Chat/routes/api.php
+++ backend-api/Modules/Chat/routes/api.php
@@ -5,4 +5,14 @@ use Modules\Chat\Http\Controllers\ChatController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
     Route::apiResource('chats', ChatController::class)->names('chat');
+
+    // 1. Tekan tombol "Chat" dari halaman detail barang
+    Route::post('/items/{itemId}/conversations', [ChatController::class, 'initiateConversation']);
+
+    // 2. Halaman Inbox (Melihat daftar semua chat milik user)
+    Route::get('/conversations', [ChatController::class, 'myConversations']);
+
+    // 3. Masuk ke dalam Room Chat (Lihat pesan & Balas pesan)
+    Route::get('/conversations/{conversationId}/messages', [ChatController::class, 'getMessages']);
+    Route::post('/conversations/{conversationId}/messages', [ChatController::class, 'sendMessage']);
 });
