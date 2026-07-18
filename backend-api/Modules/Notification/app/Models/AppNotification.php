491f89602f74edadf36faddaabd75198177acc67
AchmadZackyGZ <achmadzacky458@gmail.com>
2026-07-18 11:19:06 +0700

    menambahkan fillable apa saja di notification dan juga menambahkan relasi nya yang telah kita buat di table migration buat relasi nya di Orm models ini folder nya btw

--- backend-api/Modules/Notification/app/Models/AppNotification.php
+++ backend-api/Modules/Notification/app/Models/AppNotification.php
@@ -2,18 +2,33 @@
 
 namespace Modules\Notification\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 // use Modules\Notification\Database\Factories\AppNotificationFactory;
+use App\Models\User;
 
 class AppNotification extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     /**
      * The attributes that are mass assignable.
      */
-    protected $fillable = [];
+    protected $fillable = [
+        'user_id',
+        'title',
+        'message',
+        'is_read',
+    ];
+
+    /**
+     * 🔗 Relasi ke Pemilik Notifikasi
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 
     // protected static function newFactory(): AppNotificationFactory
     // {
